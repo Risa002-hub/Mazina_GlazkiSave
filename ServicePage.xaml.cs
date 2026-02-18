b0b96c354882d765f9073099d9408de72b25a6b7
Risa <Kristina836@icloud.com>
2026-02-18 13:21:45 +0500

    Добавьте файлы проекта.

---
+++ ServicePage.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mazina_GlazkiSave
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicePage.xaml
+    /// </summary>
+    public partial class ServicePage : Page
+    {
+        public ServicePage()
+        {
+            InitializeComponent();
+            var currentServices = Mazina_GLAZKIEntities1.GetContext().Agent.ToList();
+            ServiceListView.ItemsSource = currentServices;
+            ComboType.SelectedIndex = 0;
+            ComboSort.SelectedIndex = 0;
+            UpdateService();
+        }
+
+        private void UpdateService()
+        {
+            try
+            {
+                var currentServices = Mazina_GLAZKIEntities1.GetContext().Agent.ToList();
+                if (ComboType.SelectedItem !=null)
+                {
+                    string selectedType = (ComboType.SelectedItem as TextBlock).Text;
+
+                    if (selectedType != "Все типы")
+                    {
+                        currentServices = currentServices.Where(p => p.AgentTypeTitle == selectedType).ToList();
+                    }
+                }
+                if (!string.IsNullOrEmpty(TBSearch.Text))
+                {
+                    string searchText = TBSearch.Text.ToLower();
+                    string cleanedSearchPhone = searchText
+                        .Replace("+", "")
+                        .Replace("(", "")
+                        .Replace(")", "")
+                        .Replace("-", "")
+                        .Replace(" ", "");
+                        //.Replace("8", "7");
+
+                    currentServices = currentServices.Where(p =>
+                        // Поиск по названию
+                        (p.Title != null && p.Title.ToLower().Contains(searchText)) ||
+
+                        // Поиск по email
+                        (p.Email != null && p.Email.ToLower().Contains(searchText)) ||
+
+                        // Поиск по телефону
+                        (p.Phone != null && p.Phone
+                        .Replace("+", "")
+                        .Replace("(", "")
+                        .Replace(")", "")
+                        .Replace("-", "")
+                        .Replace(" ", "").
+                        //.Replace("8", "7").
+                        Contains(cleanedSearchPhone))
+                    ).ToList();
+                }
+                if (ComboSort.SelectedIndex == 0)
+                {
+
+                }
+                if (ComboSort.SelectedIndex == 2)
+                {
+                    currentServices = currentServices.OrderBy(p => p.Title).ToList();
+                }
+                if (ComboSort.SelectedIndex == 2)
+                {
+                    currentServices = currentServices.OrderByDescending(p => p.Title).ToList();
+                }
+                if (ComboSort.SelectedIndex == 3)
+                {
+                    currentServices = currentServices.OrderBy(p => p.Priority).ToList();
+                }
+                if (ComboSort.SelectedIndex == 4)
+                {
+                    currentServices = currentServices.OrderByDescending(p => p.Priority).ToList();
+                }
+                if (ComboSort.SelectedIndex == 5)
+                {
+                    currentServices = currentServices.OrderBy(p => p.Discount).ToList();
+                }
+                if (ComboSort.SelectedIndex == 6)
+                {
+                    currentServices = currentServices.OrderByDescending(p => p.Discount).ToList();
+                }
+
+
+                // Показываем результат
+                ServiceListView.ItemsSource = currentServices;
+                ServiceListView.ItemsSource = currentServices;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateService();
+        }
+
+        private void ComboSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateService();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateService();
+        }
+    }
+}
